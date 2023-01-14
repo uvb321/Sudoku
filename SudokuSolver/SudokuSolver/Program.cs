0914850e899b4d70f6af2631d957a67b45785313
uvb321 <uvb20031012@gmail.com>
2023-01-15 01:04:48 +0200

    final push of the program file, changed it a bit

--- SudokuSolver/SudokuSolver/Program.cs
+++ SudokuSolver/SudokuSolver/Program.cs
@@ -5,20 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+
+
 namespace SudokuSolver
 {
+    /// <summary>
+    /// this is the main program that the whole sudoku solver will be ran from
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*//main to run
+            //main to run
             Messages.Welcome_Message();
-            MainLoop.Main_Loop();*/
-            string str = "800000070006010053040600000000080400003000700020005038000000800004050061900002000";
-            int[][] board = Utils.ConvertStringToMat(str);
-            Sudoku s = new Sudoku(board);
-            s.Solve();
-           
+            MainLoop.Main_Loop();
         }
 
     }
